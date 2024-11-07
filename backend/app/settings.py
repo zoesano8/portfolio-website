7012ca4e1c51ad404ef0fabba9e7bc3393627dfc
Zoe Sano <zsano@Zoes-Laptop.local>
2024-11-07 14:05:56 -0700

    Updated ALLOWED_HOSTS to include EC2 IP

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.133.106.65']
 
 
 # Application definition
